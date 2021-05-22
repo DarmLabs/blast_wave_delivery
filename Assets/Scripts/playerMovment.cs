abb0fea6eab08409715c2682b615b7ab2a729977
BDeWinne <[e]@[mail].com[e]@[mail].com>
2021-05-22 17:25:09 -0300

    Scene Test - Movimiento Funcional X&Y

--- Assets/Scripts/playerMovment.cs
+++ Assets/Scripts/playerMovment.cs
@@ -6,9 +6,17 @@ public class playerMovment : MonoBehaviour
 {
     public float speed;
     public FixedJoystick fixedJoystick;
+
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
     public void FixedUpdate()
     {
         Vector3 direction = Vector3.up * fixedJoystick.Vertical + Vector3.right * fixedJoystick.Horizontal;
-        transform.Translate(direction * speed * Time.fixedDeltaTime);
+        rb.AddForce(direction * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+       // transform.Translate(direction * speed * Time.fixedDeltaTime);
     }
 }
