35fd6c26bfc50b7e4ddeae34def94fa478ee824b
BDeWinne <[e]@[mail].com[e]@[mail].com>
2021-05-27 18:12:51 -0300

    fix: spawnManager con movimiento

--- Assets/Scripts/spawnManager.cs
+++ Assets/Scripts/spawnManager.cs
@@ -22,7 +22,7 @@ public class spawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        transform.Translate(0,0, 0.1f);
     }
 
     void spawnObstacle(){
