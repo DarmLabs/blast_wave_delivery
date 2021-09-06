60f30283244f3dc4d4f1652f6848812bb240aa24
BDeWinne <68663886+BDeWinne@users.noreply.github.com>
2021-09-06 12:13:16 -0300

    change audioVisua, poscion moto, flechas movimiento

---
+++ Assets/Scripts/staticFollow.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class staticFollow : MonoBehaviour
+{
+    //Este script es para que un objecto vaya a la misma velocidad en z que la moto pero quede estatico en x e y
+    public GameObject Moto;
+    void Update()
+    {
+        transform.Translate(0,0, Moto.GetComponent<playerMovement>().speedz * (Time.deltaTime * Moto.GetComponent<playerMovement>().speedMultiplier));
+    }
+}
