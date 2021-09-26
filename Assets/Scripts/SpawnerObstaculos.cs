c1df83c64cddc5e4d142e185b748033af7f4de8f
julagui <julioaguilar-ap@outlook.com>
2021-09-26 15:54:57 -0300

    Obstaculos

--- Assets/Scripts/SpawnerObstaculos.cs
+++ Assets/Scripts/SpawnerObstaculos.cs
@@ -4,28 +4,30 @@ using UnityEngine;
 
 public class SpawnerObstaculos : MonoBehaviour
 {
-    public GameObject[] Obstaculos;
-    public Transform[] spawnPoints;
-    private int indexObstaculos = 6;
-    private int indexPosicion = 6;
+    public GameObject[] ObstaculosVan;
+    public GameObject[] ObstaculosVienen;
+    public Transform[] spawnPointsVan;
+    public Transform[] spawnPointsVienen;
+    private int indexObstaculosVan = 3;
+    private int indexObstaculosVienen = 3;
+    private int indexPosicionVan = 3;
+    private int indexPosicionVienen = 3;
     public float timeSpawn = 0.4f;
 
     
     void Start () 
     {
         //se instancia la comida que cae, con una diferencia de tiempo, lo hace aleatoriamente
-        InvokeRepeating("invocarComida", 0.1f, timeSpawn);
+        InvokeRepeating("invocarObstaculos", 0.1f, timeSpawn);
         
     }
+    
 
-    void Update()
+    void invocarObstaculos ()
     {
-        
-    }
-
-    void invocarComida ()
-    {
-        Instantiate(Obstaculos[Random.Range(0,indexObstaculos)],spawnPoints[Random.Range(0,indexPosicion)].position,transform.rotation);
+        //Instantiate(Obstaculos[Random.Range(0,indexObstaculos)],spawnPoints[Random.Range(0,indexPosicion)].position,transform.rotation);
+        Instantiate(ObstaculosVienen[Random.Range(0,indexObstaculosVienen)],spawnPointsVienen[Random.Range(0,indexPosicionVienen)].position,transform.rotation);
+        Instantiate(ObstaculosVan[Random.Range(0,indexObstaculosVan)],spawnPointsVan[Random.Range(0,indexPosicionVan)].position,transform.rotation);
     }
    
 }
