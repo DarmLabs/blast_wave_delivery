edd68c47d3b5d67030277734ae5ab6a2bec4edaf
BDeWinne <68663886+BDeWinne@users.noreply.github.com>
2021-10-22 11:53:32 -0300

    interfaz tienda

--- Assets/Scripts/UI/Main_Menu_Manager.cs
+++ Assets/Scripts/UI/Main_Menu_Manager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class Main_Menu_Manager : MonoBehaviour
 {
@@ -24,6 +25,11 @@ public class Main_Menu_Manager : MonoBehaviour
     GameObject slider;
     bool music;
     bool sfx;
+    //Tienda
+    public Text descriptionPot;
+    public GameObject buyButtonPot;
+    public Text descriptionObj;
+    public GameObject buyButtonObj;    
     void Start()
     {
         Time.timeScale = 1;
@@ -108,7 +114,7 @@ public class Main_Menu_Manager : MonoBehaviour
             pjSection.transform.SetSiblingIndex(1);
             musicSection.transform.SetSiblingIndex(0);
         }
-        if(buttonPressed == "Pj")
+        if(buttonPressed == "Cosmetics")
         {
             pjSection.transform.SetSiblingIndex(2);
         }
@@ -150,5 +156,37 @@ public class Main_Menu_Manager : MonoBehaviour
             sfx = false;
             slider.GetComponent<Animator>().Play("Desliz 0");
         }
+    }
+
+    //Tienda
+
+    public void Potenciadores()
+    {
+        buttonPressed = EventSystem.current.currentSelectedGameObject.name;
+        TextAsset file = Resources.Load<TextAsset>("ModesDesc/"+buttonPressed+"Button");
+        if(file != null)
+        {
+            descriptionPot.GetComponent<Text>().text = file.text;
+        }
+        buyButtonPot.SetActive(true);
+    }
+    public void Objetos()
+    {
+        buttonPressed = EventSystem.current.currentSelectedGameObject.name;
+        TextAsset file = Resources.Load<TextAsset>("DescObjetos/"+buttonPressed);
+        if(file != null)
+        {
+            descriptionObj.GetComponent<Text>().text = file.text;
+        }
+        buyButtonObj.SetActive(true);
+    }
+    public void Cosmeticos()
+    {
+        buttonPressed = EventSystem.current.currentSelectedGameObject.name;
+        ConfirmationScreen();
+    }
+    public void ConfirmationScreen()
+    {
+        
     }
 }
