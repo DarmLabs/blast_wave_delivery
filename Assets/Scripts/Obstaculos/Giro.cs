63ef9a40f3b0693539259a0e3bfdd417cef497a0
julagui <julioaguilar-ap@outlook.com>
2021-10-26 23:29:47 -0300

    Prefab pizza

---
+++ Assets/Scripts/Obstaculos/Giro.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Giro : MonoBehaviour
+{
+    
+    void Update()
+    {
+        transform.Rotate(0,1.8f,0);
+    }
+}
